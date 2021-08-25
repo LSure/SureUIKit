3a2f2a2a34a9e22b794665e25f282af20588c561
liushuo <liushuo.sure@bytedance.com>
2021-08-25 18:32:51 +0800

    [feat] add file

---
+++ SureUIKit/Classes/SureAlertView.m
@@ -0,0 +1,19 @@
+//
+//  SureAlertView.m
+//  SureUIKit
+//
+//  Created by bytedance on 2021/8/25.
+//
+
+#import "SureAlertView.h"
+
+@implementation SureAlertView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+@end
---
+++ SureUIKit/Classes/SureToastView.m
@@ -0,0 +1,18 @@
+//
+//  SureToastView.m
+//  SureUIKit
+//
+//  Created by bytedance on 2021/8/25.
+//
+
+#import "SureToastView.h"
+
+@implementation SureToastView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+@end
